569ef750bc4d48daf093a2e8dad30656434d52f1
Yujin <jin37091289@gmail.com>
2023-04-13 19:06:10 +0900

    UPDATE : 파일명 수정

--- Day01/ch02_test2.js
+++
@@ -1,9 +0,0 @@
-console.log('argv 속성의 파라미터 수 : ' + process.argv.length)
-console.dir(process.argv)   // 배열로 되어있다
-
-// process도 전역변수이다
-
-
-
-
-
--- Day01/ch02_test5.js
+++
@@ -1,8 +0,0 @@
-var calc2 = require('./calc2')
-
-console.log('모듈로 분리한 후 - calc2.add : ' + calc2.add(30, 30))
-
-var nconf = require('nconf')        // 외부 모듈이라서 npm install 모듈명으로 다운로드
-nconf.env()
-var value = nconf.get('OS')
-console.log('OS 환경변수의 값 : ' + value)
